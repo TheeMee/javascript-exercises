352d504711dea6401ac8321f13492bbcfc594da3
Teerath <themetheetath@gmail.com>
2024-07-12 10:07:43 +0700

    completed tempConversion exercise encountered problem: toFixed returns string

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,26 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempinFarenheit) {
+  let tempinCelcius = (tempinFarenheit - 32) * (5/9);
+  
+  if (tempinCelcius % 1 === 0) {
+    return Number(tempinCelcius);
+  } else {
+    tempinCelcius = tempinCelcius.toFixed(1);
+    return Number(tempinCelcius);
+  }
+
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(tempinCelcius) {
+  let tempinFarenheit = (tempinCelcius * (9/5)) + 32;
+  
 
+  if (tempinFarenheit % 1 === 0) {
+    return Number(tempinFarenheit);
+  } else {
+    tempinFarenheit = tempinFarenheit.toFixed(1);
+    return Number(tempinFarenheit);
+  }
+};
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
