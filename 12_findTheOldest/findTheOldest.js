cdcb43543e0e0c89cd1385a16e6a37bbeffad216
Teerath <themetheetath@gmail.com>
2024-07-20 23:44:06 +0700

    completed all assignments

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let oldestPerson = {};
+    const date = new Date();
 
-};
+    console.log(people);
+
+    for (personIndex in people) {
+        console.log('iterate')
+        person = people[personIndex];
+        console.log(typeof(person));
+        console.log('person', person);
+        let age = 0;
+
+        if (person.yearOfDeath) {
+            age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            age = date.getFullYear() - person.yearOfBirth;
+        }
+
+        console.log('age, oldest age', age, oldestAge)
+        console.log(age > oldestAge);
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = person;
+            console.log('oldest: ', oldestPerson);
+        }
+    };
+    return oldestPerson;
+    };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
